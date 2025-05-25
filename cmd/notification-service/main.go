5185a37e8a352b31b0971d2d0d65086e5b735920
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2025-05-25 10:18:53 -0700

    fixing grpc port

--- cmd/notification-service/main.go
+++ cmd/notification-service/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -27,18 +26,26 @@ func main() {
 		ServiceName: cfg.Log.ServiceName,
 	})
 	emailSvc := email.NewService(cfg)
-	// pushSvc, err := firebase.NewService(cfg)
-
+	// Create gRPC server
 	grpcServer := rpc.NewServer(emailSvc)
+
+	// Start server in a goroutine
 	go func() {
-		if err := grpcServer.Start(cfg.Server.Port); err != nil {
-			fmt.Println("error occured", err.Error())
+		if err := grpcServer.Start(cfg.GRPC.Port); err != nil {
 		}
 	}()
+
+	// Wait for interrupt signal
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive a signal
+	<-quit
+
+	// Create shutdown context with timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	grpcServer.Stop(ctx)
 
+	// Gracefully stop the server
+	grpcServer.Stop(ctx)
 }
