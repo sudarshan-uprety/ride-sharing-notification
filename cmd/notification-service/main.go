6b3c2ea859c1539f7aa325c4fbe3e885abb71454
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2025-05-25 10:05:13 -0700

    main.go fixing

--- cmd/notification-service/main.go
+++ cmd/notification-service/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"ride-sharing-notification/config"
 	"ride-sharing-notification/internal/delivery/rpc"
 	"ride-sharing-notification/internal/pkg/email"
 	"ride-sharing-notification/internal/pkg/logging"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -23,5 +29,16 @@ func main() {
 	emailSvc := email.NewService(cfg)
 	// pushSvc, err := firebase.NewService(cfg)
 
-	grpcServer := rpc.NewServer(emailSvc, rpc.EmailServiceClient)
+	grpcServer := rpc.NewServer(emailSvc)
+	go func() {
+		if err := grpcServer.Start(cfg.Server.Port); err != nil {
+			fmt.Println("error occured", err.Error())
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	grpcServer.Stop(ctx)
+
 }
